3b1ab522e38240e6a03fe4939da3ae4398c2ac4e
Stan-Sasquatch <stephenppercival@gmail.com>
2019-10-20 16:43:44 +0100

    added date sorting (US)

--- src/User.js
+++ src/User.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const User = (props) => {
     console.log(props);
-    return (<div>{props.user["id"] + ": " + props.user["last_name"] + ", " + props.user["Country"]}; date joined: {props.user["Date Joined"]}</div>);
+    return (<div>{props.user["last_name"] + ", " + props.user["Country"]}; date joined: {props.user["Date Joined"]}</div>);
 }
 
 export default User;
--- src/UsersPage.js
+++ src/UsersPage.js
@@ -12,7 +12,8 @@ class UsersPage extends React.Component {
             numberOfUsers: null,
             numberOfPages: 0,
             currentPage: 0,
-            numberOfUsersToDisplay: 10
+            numberOfUsersToDisplay: 10,
+            sortByDate: false
         }
     }
 
@@ -29,22 +30,53 @@ class UsersPage extends React.Component {
     initialiseUserPage = (json, numUsersToDisplay) => {
 
         const pages = Math.ceil(json.length / numUsersToDisplay)
-        const startPage = 0
+        const startPage = 0;
 
-        const sortedUsers = json.sort(function (a, b) {
-            if (a["last_name"] < b["last_name"]) { return -1; }
-            if (a["last_name"] > b["last_name"]) { return 1; }
-            return 0;
+
+        const sortedUsers =
+
+
+            !this.state.sortByDate ? json.sort(function (a, b) {
+                if (a["last_name"] < b["last_name"]) { return -1; }
+                if (a["last_name"] > b["last_name"]) { return 1; }
+                return 0;
+            }) : json.sort(function (a, b) {
+                let toDateObject = (stringDate) => {
+                    let slashPosition = []
+                    for (let i = 0; i < stringDate.length; i++) {
+                        if (stringDate.charAt(i) == "/") {
+                            slashPosition.push(i);
+                        }
+                    }
+
+                    let month = stringDate.substring(0, slashPosition[0]) - 1;
+
+
+                    let day = stringDate.substring(slashPosition[0] + 1, slashPosition[1]);
+
+                    let year = stringDate.substring(slashPosition[1] + 1);
+
+                    let dateObject = [year, month, day]
+
+                    return dateObject
+                }
+                return new Date(...toDateObject(a["Date Joined"])) - new Date(...toDateObject(b["Date Joined"]))
+            });
+
+
+
+
+
+
+
+
+        this.setState({
+            allUsers: sortedUsers, loaded: true, numberOfUsers: json.length, numberOfPages: pages, currentPage: startPage, numberOfUsersToDisplay: numUsersToDisplay
         })
-
-
-
-        this.setState({ allUsers: sortedUsers, loaded: true, numberOfUsers: json.length, numberOfPages: pages, currentPage: startPage, numberOfUsersToDisplay: numUsersToDisplay })
     }
     pageListOnClick = (pageNum) => {
         this.setState({ currentPage: pageNum })
     }
-
 
     render() {
         if (!this.state.loaded) { return <div>Loading...</div> }
