f5d4003f5122d486b0354fc718405efaaac94199
Stan-Sasquatch <stephenppercival@gmail.com>
2019-10-20 12:31:01 +0100

    NavBar Css

--- src/User.js
+++ src/User.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const User = (props) => {
     console.log(props);
-    return (<div>{props.user["last_name"]}</div>);
+    return (<div>{props.user["id"] + ": " + props.user["last_name"] + ", " + props.user["Country"]}</div>);
 }
 
 export default User;
